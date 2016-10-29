3eca96582c3155f6cde9cc943296de8ab642a703
Juliette Malassé <juliette.malasse@epita.fr>
2016-10-29 18:56:22 +0200

    2nd commit NeuralNetHeader

---
+++ OCRap/NeuronalNetwork.h
@@ -0,0 +1,17 @@
+#ifndef NEURONAL_NETWORK_H_
+# define NEURONAL_NETWORK_H_
+
+typedef struct __attribute__((packed))
+{
+  int                 _input;
+  int                 _output;
+  int                 _hidden;
+}                     NeuralNertworkInit;
+
+typedef struct __attribute__ ((packed))
+{
+  
+}
+
+
+#endif /* !NEURONAL_NETWORK_H_ */
