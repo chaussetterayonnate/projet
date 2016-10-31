b93b8b0a9716f6521e14836033278646e5156d4c
Valentine Bernard <valentine.bernard@epita.fr>
2016-10-31 12:10:32 +0100

    xor cost function

--- OCRap/xorPrototype.c
+++ OCRap/xorPrototype.c
@@ -65,30 +65,35 @@ for(size_t i = 0; i < 4; i++)
     
   }
 
-printf("bias_1a = ");
+printf("\nbias_1a = ");
  for(size_t j = 0; j < 4; j++)
    printf("%.2f, ", *(net->bias_1a + j));
  
- printf("bias_1b = ");
+ printf("\nbias_1b = ");
  for(size_t j = 0; j < 4; j++)
    printf("%.2f, ", *(net->bias_1b + j));
 
- printf("bias_2 = ");
+ printf("\nbias_2 = ");
  for(size_t j = 0; j < 4; j++)
    printf("%.2f, ", *(net->bias_2 + j));
  
- printf("weight_1a = ");
+ printf("\nweight_1a = ");
 for(size_t j = 0; j < 4; j++)
     printf("%.2f, ", *(net->weight_1a + j));
 
  printf("\nweight_1b = ");
  for(size_t j = 0; j <4; j++)
    printf("%.2f, ", *(net->weight_1b + j));
+
+ printf("\nweight_2 = ");
+ for(size_t j = 0; j <4; j++)
+   printf("%.2f, ", *(net->weight_2 + j));
+ 
  
  return net;
 }
 
-void forward(NeuralNetworkInit *net)
+float* forward(NeuralNetworkInit *net)
 {
   /*list of hidden neurons for
    the test a = 0, b=0*/
@@ -98,12 +103,19 @@ void forward(NeuralNetworkInit *net)
   float        *z1_10;
   float        *z1_11;
 
+  float        output_00 = 0;
+  float        output_01 = 0;
+  float        output_10 = 0;
+  float        output_11 = 0;
+  
+  float        *output;
+
   z1_00 = malloc(4*sizeof(float));
   z1_01 = malloc(4*sizeof(float));
   z1_10 = malloc(4*sizeof(float));
   z1_11 = malloc(4*sizeof(float));
 
-  
+  output = malloc(4*sizeof(float));
 
   for(size_t i = 0; i < 4; i++)
     {
@@ -138,17 +150,77 @@ void forward(NeuralNetworkInit *net)
   for (size_t j = 0; j < 4; j++)
     printf("%.2f, ",*(z1_11+j));
 
+  for(size_t i = 0; i < 4; i++)
+    {
+      output_00 += *(z1_00 +i)*(*(net->weight_2 +i))
+	+ *(net->bias_2 +i);
+      output_01 += *(z1_01 +i)*(*(net->weight_2 +i))
+	+ *(net->bias_2 +i);
+      output_10 += *(z1_10 +i)*(*(net->weight_2 +i))
+	+ *(net->bias_2 +i);
+      output_11 += *(z1_11 +i)*(*(net->weight_2 +i))
+	+ *(net->bias_2 +i);
+    }
+
+  *output   = sigmoid(output_00);
+  *(output+1)= sigmoid(output_01);
+  *(output +2) = sigmoid(output_10);
+  *(output +3) = sigmoid(output_11);
+  
+  printf("\n00 : %.2f \n", output_00);
+  printf("01 : %.2f \n", output_01);
+  printf("10 : %.2f \n", output_10);
+  printf("11 : %.2f \n", output_11);
+
+  return output;
 }
+
+float * costFunction(float *output)
+{
+  float *cost;
+  cost = malloc(4 * sizeof(float));
+
+  *cost = pow(*output,2)/2;
+  *(cost+1) = pow(1 - *(output+1),2)/2;
+  *(cost+2) = pow(1 - *( output+2),2)/2;
+  *(cost+3) = pow(*(output+3),2)/2;
+
+  printf("\ncost = ");
+  for(size_t i = 0; i <4; i++)
+    printf("%.5f, ", *(cost+i));
+  
+  return cost;
+}
+
+/*float * deriv_w2(NeuralNetworkInit net, float *output)
+{
+  
+}
+
+float * deriv_b2(NeuralNetworkInit net, float *output)
+{}
+
+float * deriv_w1(NeuralNetworkInit net, float *output)
+{}
+
+float * deriv_b1(NeuralNetworkInit net, float *output)
+{}*/
+
+
 
 int main(void)
 {
   NeuralNetworkInit    *ne = init_network();
   if (!ne)
     return -1;
+  
+  float *output;
+  output = malloc(4*sizeof(float));
 
-
-  forward(ne);
-
+  output = forward(ne);
+  costFunction(output);
+  
   free(ne);
+  free(output);
   return EXIT_SUCCESS;
 }
