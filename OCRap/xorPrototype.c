6127c1a10f426dcdc5bbd2215ac08f7f4abe62b9
Valentine Bernard <valentine.bernard@epita.fr>
2016-10-31 05:55:26 +0100

    xor forward

--- OCRap/xorPrototype.c
+++ OCRap/xorPrototype.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <unistd.h>
 #include "NeuronalNetwork.h"
 
-#define random() (float)rand()/(float)(RAND_MAX)
+#define random()         (float)rand()/(float)(RAND_MAX)
+#define expo(z)          exp(-(double)z)
+#define sigmoid(z)       (1. / (1. + expo(z))
+#define sigmoidDer(z)    ((float)(z)*(1.-z))
 
 NeuralNetworkInit     *init_network(void)
 {
@@ -44,11 +47,64 @@ for(size_t j = 0; j < 4; j++)
  return net;
 }
 
+void forward(NeuralNetworkInit *net, int *a, int *b)
+{
+  /*list of hidden neurons for
+   the test a = 0, b=0*/
+  float        *z1_00;
+  //same for different a & b
+  float        *z1_01;
+  float        *z1_10;
+  float        *z1_11;
+
+  z1_00 = malloc(4*sizeof(float));
+  z1_01 = malloc(4*sizeof(float));
+  z1_10 = malloc(4*sizeof(float));
+  z1_11 = malloc(4*sizeof(float));
+
+  
+  for(size_t i = 0; i < 4; i++)
+    {
+      *(z1_00 + i) = sigmoid(((*(a+i)) * (*(net->weight_1a + i)) + net->bias) +
+			     ((*(b+i)) * (*(net->weight_1b + i)) + net->bias)));
+      *(z1_01 + i) = sigmoid(((*(a+i)) * (*(net->weight_1a + i)) + net->bias) +
+			     ((*(b+i)) * (*(net->weight_1b + i)) + net->bias)));
+      *(z1_10 + i) = sigmoid(((*(a+i)) * (*(net->weight_1a + i)) + net->bias) +
+			     ((*(b+i)) * (*(net->weight_1b + i)) + net->bias)));
+      *(z1_11 + i) = sigmoid(((*(a+i)) * (*(net->weight_1a + i)) + net->bias) +
+			     ((*(b+i)) * (*(net->weight_1b + i)) + net->bias)));
+    }
+  printf("z1_00 = ");
+  for(size_t j = 0; j < 4; j++)
+    {
+      printf("%.2f, ",*(z1_00+j));
+    }
+}
+
 int main(void)
 {
   NeuralNetworkInit    *ne = init_network();
   if (!ne)
     return -1;
+  
+  int *a;
+  int *b;
+  a = malloc(4*sizeof(int));
+  b = malloc(4*sizeof(int));
+
+  *a = 0;
+  *b = 0;
+  *(a+1) = 0;
+  *(b+1) = 1;
+  *(a+2) = 1;
+  *(b+2) = 0;
+  *(a+3) = 1;
+  *(b+3) = 1;
+
+  forward(ne,a,b);
+  
   free(ne);
+  free(a);
+  free(b);
   return EXIT_SUCCESS;
 }
